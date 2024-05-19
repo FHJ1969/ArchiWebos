748781c5cacbcaa13b04d44415243300f3a00202
Nassim <achnassim34@gmail.com>
2024-05-19 23:36:05 +0200

    Ajout de la gallery dynamique et début création des filtres

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,4 +1,49 @@
-const reponse = await fetch("http://localhost:5678/api/works")
-const works = await reponse.json()
-console.log(works)
+//Appel des API en JSON
+
+const reponseWorks = await fetch("http://localhost:5678/api/works")
+const works = await reponseWorks.json()
+
+const reponseCategories = await fetch("http://localhost:5678/api/categories")
+const categories = await reponseCategories.json()
+console.log(categories)
+
+
+//Récupération de la .gallery et du .filtres du html
+let gallery = document.querySelector (".gallery");
+let filtres = document.querySelector (".liste-filtres");
+
+
+
+//Création des élements Filtres
+function generationFiltres(){
+    for (let i=0; i < categories.length; i++){
+        let filtre =  document.createElement("button");
+        filtre.classList.add("filtre" + [i],"filtre")
+        filtre.innerText = categories[i].name;
+        filtres.appendChild(filtre);
+    }
+}
+
+
+//Création des élements Gallery
+function generationGallery() {
+    for (let i = 0; i < works.length; i++) {
+        let boite = document.createElement("figure");
+        gallery.appendChild(boite)
+
+        let image = document.createElement("img")
+        image.src = works[i].imageUrl;
+        boite.appendChild(image)
+
+        let titre = document.createElement("figcaption");
+        titre.innerText = works[i].title;
+        boite.appendChild(titre);
+    }
+}
+
+
+//Appel des fonctions
+generationFiltres();
+generationGallery();
+
 
