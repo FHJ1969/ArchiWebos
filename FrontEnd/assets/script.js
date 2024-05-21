315b09f0f879074fe3cda29c901113c6b3b9e777
Nassim <achnassim34@gmail.com>
2024-05-21 18:04:56 +0200

    Reglage des bugs filtres et filtre Tous fonctionnel

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,24 +1,21 @@
 // Appel des API en JSON
 const reponseWorks = await fetch("http://localhost:5678/api/works");
 const works = await reponseWorks.json();
-
 const reponseCategories = await fetch("http://localhost:5678/api/categories");
 const categories = await reponseCategories.json();
 
-// Récupération de la galerie et du conteneur des filtres du HTML
+// Récupération de la galerie et des filtres
 let gallery = document.querySelector(".gallery");
 let filtresContainer = document.querySelector(".liste-filtres");
 
 // Création des éléments filtres
 function generationFiltres() {
-    // Création du filtre "Tous"
     let filtreTous = document.createElement("button");
     filtreTous.classList.add("filtre", "filtre-all", "filtre-selection");
     filtreTous.innerText = "Tous";
     filtreTous.type = "button";
     filtresContainer.appendChild(filtreTous);
 
-    // Création des filtres pour chaque catégorie
     for (let i = 0; i < categories.length; i++) {
         let filtre = document.createElement("button");
         filtre.classList.add("filtre", "filtre-" + categories[i].id);
@@ -45,29 +42,23 @@ function generationGallery() {
     }
 }
 
-// Appel des fonctions
+// Appel des fonctions pour l'affichage dynamique des filtres et de la gallery
 await generationFiltres();
 await generationGallery();
 
-// Récupération des boutons de filtre
+// Récupération des bouttons de filtre
 const btnTous = document.querySelector(".filtre-all");
 const listeBtn = document.querySelectorAll(".filtre");
 
-// Ajout d'un écouteur d'événement sur chaque bouton de filtre
+//Affichage de la gallery approprié selon le filtre pressé
 listeBtn.forEach((btn) => {
-    btnTous.addEventListener("click", ()=>{
-        filtresContainer.innerHTML = "";
-        generationFiltres()
-        gallery.innerHTML = "";
-        generationGallery();
-    });
     btn.addEventListener("click", () => {
-        // Suppression de la classe "filtre-selection" sur tous les boutons
+        // Supprimer de la classe "filtre-selection" sur tous les bouttons
         listeBtn.forEach((btn) => {
             btn.classList.remove("filtre-selection");
         });
 
-        // Ajout de la classe "filtre-selection" sur le bouton cliqué
+        // Réatributtion de la classe "filtre-selection"
         btn.classList.add("filtre-selection");
 
         // Masquage de toutes les boites
@@ -81,12 +72,19 @@ listeBtn.forEach((btn) => {
         boitesFiltre.forEach((boite) => {
             boite.style.display = "block";
         });
-
-        // Si le bouton "Tous" est cliqué, affichage de toutes les boites
-        if (btn.classList[1] === "all") {
-            boites.forEach((boite) => {
-                boite.style.display = "block";
-            });
-        }
     });
+});
+
+//Affichage à part pour le filtre "Tous"
+btnTous.addEventListener("click", () => {
+    // Afficher tous les éléments de la gallery après le click
+    const galleryItems = document.querySelectorAll(".gallery figure");
+    for (let i = 0; i < galleryItems.length; i++) {
+        galleryItems[i].style.display = "block";
+    }
+
+    // Réinitialiser le filtre-selection
+    const filtreSelection = document.querySelector(".filtre-selection");
+    filtreSelection.classList.remove("filtre-selection");
+    btnTous.classList.add("filtre-selection");
 });
