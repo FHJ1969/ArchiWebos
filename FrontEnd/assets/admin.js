067ae09a8c1366c462c11e607b456ce45afe7b3f
Nassim <achnassim34@gmail.com>
2024-05-29 09:19:25 +0200

    Ajout du bouton de la modale

--- FrontEnd/assets/admin.js
+++ FrontEnd/assets/admin.js
@@ -2,11 +2,25 @@
 const loginElement = document.querySelector('.login');
 const listeFiltres = document.querySelector('.liste-filtres');
 
-if (localStorage.getItem('userConnected')){
+if (localStorage.getItem('userConnected')) {
+//Eléments modifié avec le passage en page admin
     loginElement.innerText = "logout"
     loginElement.href = "index.html";
     listeFiltres.style.display = "none";
+//Création du bouton de la modale "modifier"
+    const mesProjets = document.querySelector('#portfolio h2');
+
+    const modifierIcone = document.createElement("i");
+    modifierIcone.className = "fa-regular fa-pen-to-square";
+    mesProjets.appendChild(modifierIcone);
+
+    const modifier = document.createElement("button");
+    modifier.innerText = "modifier";
+    mesProjets.appendChild(modifier);
+    modifier.classList.add('btn-modifier')
+
 } else {
+//Affichage normal si non connecté
     loginElement.innerText = "login"
     loginElement.href = "index-login.html";
 }
