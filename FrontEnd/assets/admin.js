37c8c6ef9bd6a5afddbfa675cb0bf26e9f0a6e5f
Nassim <achnassim34@gmail.com>
2024-05-31 00:28:01 +0200

    Finition de la modale supprimer(pb d'author à fix) et début modale ajout photo

--- FrontEnd/assets/admin.js
+++ FrontEnd/assets/admin.js
@@ -1,6 +1,3 @@
-const reponseWork = await fetch("http://localhost:5678/api/works");
-const work = await reponseWork.json();
-//Modification des éléments selon la présence ou non de 'userConnected' en localStorage
 const loginElement = document.querySelector('.login');
 const listeFiltres = document.querySelector('.liste-filtres');
 
@@ -35,44 +32,4 @@ loginElement.addEventListener('click', (event) => {
     }
 })
 
-// Sélectionnez le bouton modifier et la boîte de dialogue modale
-const btnModifier = document.querySelector('.btn-modifier');
-const modal = document.getElementById('modal');
-
-// Ajoutez un événement clic au bouton modifier
-btnModifier.addEventListener('click', function() {
-    // Ouvrez la boîte de dialogue modale
-    modal.style.display = "block";
-});
-
-// Sélectionnez le bouton de fermeture et ajoutez un événement clic
-const span = document.querySelector(".fa-xmark");
-span.addEventListener('click', function() {
-    // Fermez la boîte de dialogue modale
-    modal.style.display = "none";
-});
-
-// Fermez la boîte de dialogue modale lorsque l'utilisateur clique en dehors de celle-ci
-window.addEventListener('click', function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-});
-
-const listeBoites = document.querySelector('.modale-liste-boites');
-function galleryModale() {
-    for (let i = 0; i < work.length; i++) {
-        const boiteModale = document.createElement("figure");
-        boiteModale.classList.add('boite-modale')
-        listeBoites.appendChild(boiteModale);
-        //boite.classList.add("boite", "boite-" + works[i].categoryId);
-        boiteModale.dataset.categoryId = work[i].categoryId
-        let image = document.createElement("img");
-        image.src = work[i].imageUrl;
-        boiteModale.appendChild(image);
-
-        listeBoites.appendChild(boiteModale);
-    }
-}
-galleryModale()
 
---
+++ FrontEnd/assets/modale.js
@@ -0,0 +1,84 @@
+const reponseWork = await fetch("http://localhost:5678/api/works");
+const work = await reponseWork.json();
+const loginElement = document.querySelector('.login');
+const listeFiltres = document.querySelector('.liste-filtres');
+//Création de la page modale
+const btnModifier = document.querySelector('.btn-modifier');
+const modal = document.getElementById('modal');
+const modaleTitre = document.querySelector('.modal-content h3');
+const ajoutPhoto = document.querySelector('.modal-content h4');
+
+btnModifier.addEventListener('click', function() {
+    // Ouvrez la boîte de dialogue modale
+    modal.style.display = "block";
+});
+
+const span = document.querySelector(".fa-xmark");
+span.addEventListener('click', function() {
+    // Fermez la boîte de dialogue modale
+    modal.style.display = "none";
+    // Réinitialisez le contenu d'origine
+    modaleTitre.innerText = "Galerie photo";
+    ajoutPhoto.innerText = "Ajouter une photo";
+});
+
+// Fermer la modale lorsqu'on clique en dehors de celle-ci
+window.addEventListener('click', function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+        // Réinitialisez le contenu d'origine
+        modaleTitre.innerText = "Galerie photo";
+        ajoutPhoto.innerText = "Ajouter une photo";
+    }
+});
+
+//Génération de la modale
+const listeBoites = document.querySelector('.modale-liste-boites');
+function galleryModale() {
+    for (let i = 0; i < work.length; i++) {
+        const boiteModale = document.createElement("figure");
+        boiteModale.classList.add("boite-modale");
+        listeBoites.appendChild(boiteModale);
+        boiteModale.dataset.categoryId = work[i].categoryId;
+
+        const image = document.createElement("img");
+        image.src = work[i].imageUrl;
+        boiteModale.appendChild(image);
+
+        const iconeSupprimer = document.createElement("i");
+        iconeSupprimer.className = "fa-solid fa-trash-can";
+        boiteModale.appendChild(iconeSupprimer);
+
+        //Suppression des work selon le bouton supprimer pressé
+        iconeSupprimer.addEventListener('click', (event) => {
+            const workId = boiteModale.dataset.id;
+
+            fetch(`http://localhost:5678/api/works/${workId}`, {
+                method: 'DELETE',
+                headers: {
+                    'accept': '*/*',
+                    'Authorization': `Bearer ${localStorage.getItem('token')}`
+                }
+            })
+                .then(response => {
+                    if (response.ok) {
+                        boiteModale.remove();
+                    } else {
+                        console.error('Erreur dans la suppression:', response);
+                    }
+                })
+                .catch(error => {
+                    console.error('Erreur dans la suppression:', error);
+                });
+        });
+        //Changement des élements de la modale après que le bouton "Ajouter une photo soit pressé"
+        ajoutPhoto.addEventListener('click', (event) => {
+            event.preventDefault();
+            modaleTitre.innerText = "Ajout photo";
+            ajoutPhoto.innerText = "Valider";
+            listeBoites.style.display = "none";
+        })
+    }
+}
+
+galleryModale()
