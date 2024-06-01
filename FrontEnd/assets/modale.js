d2405590e08904c6efd1d32290c0f6cc71376b23
Nassim <achnassim34@gmail.com>
2024-06-01 10:19:19 +0200

    Debuggage et ajout de fonctionnalité de la modale Ajout photo

--- FrontEnd/assets/modale.js
+++ FrontEnd/assets/modale.js
@@ -1,5 +1,6 @@
 const reponseWork = await fetch("http://localhost:5678/api/works");
 const work = await reponseWork.json();
+//Définition des élements du DOM
 const btnModifier = document.querySelector('.btn-modifier');
 const modal = document.getElementById('modal');
 const modaleTitre = document.querySelector('.modal-content h3');
@@ -8,7 +9,12 @@ const formulaireModale = document.querySelector('.formulaire-modale');
 const btnRetour = document.querySelector('.fa-arrow-left');
 const fermer = document.querySelector(".fa-xmark");
 const modaleHeader = document.querySelector('.modal-header');
+const inputTitre = document.getElementById('titre')
+const inputPhoto = document.querySelector('#photo-input');
+const inputCateogorie = document.getElementById('categorie');
+const photoUpload = document.querySelector('.photo-upload');
 
+//Functions à utiliser plusieurs fois durant le code
 function ajoutRetour() {
     if (formulaireModale.style.display === "block"){
         btnRetour.style.display = "block";
@@ -27,24 +33,26 @@ function resetModal() {
     listeBoites.style.display = "";
     ajoutRetour()
 }
-
+//Changement de status selon le bouton pressé
 btnModifier.addEventListener('click', function() {
     modal.style.display = "block";
     ajoutRetour();
 });
-// Fermez la modale quand l'icone X est pressé
+
 fermer.addEventListener('click', function() {
     modal.style.display = "none";
     resetModal()
 });
-// Fermer la modale lorsqu'on clique en dehors de celle-ci
+
 window.addEventListener('click', function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
         resetModal()
     }
 });
-//Génération de la modale
+btnRetour.addEventListener('click', resetModal)
+
+//Génération de la modale et ses changements de status
 const listeBoites = document.querySelector('.modale-liste-boites');
 function galleryModale() {
     for (let i = 0; i < work.length; i++) {
@@ -83,47 +91,46 @@ function galleryModale() {
                     console.error('Erreur dans la suppression:', error);
                 });
         });
-
-        //Changement des élements de la modale après que le bouton "Ajouter une photo soit pressé"
-        ajoutPhoto.addEventListener('click', (event) => {
-            modaleTitre.innerText = "Ajout photo";
-            ajoutPhoto.innerText = "Valider";
-            ajoutPhoto.style.backgroundColor = "#A7A7A7";
-            ajoutPhoto.style.cursor = "not-allowed"
-            listeBoites.style.display = "none";
-            formulaireModale.style.display = "block";
-            ajoutRetour();
-            //if (inputPhoto !== null && inputTitre !== null) {
-                //ajoutPhoto.style.backgroundColor = "#1d6154";
-                //ajoutPhoto.style.cursor = "pointer"
-            //}
-        })
-
-        btnRetour.addEventListener('click', (event) => {
-            resetModal()
-        })
-
-        //Ajout de la photo sélectionné comme placeholder
-        const inputTitre = document.getElementById('titre')
-        const inputPhoto = document.querySelector('#photo-input');
-        const photoUpload = document.querySelector('.photo-upload');
-
-
-        inputPhoto.addEventListener('change', function(event) {
-            const file = event.target.files[0];
-            const reader = new FileReader();
-
-            reader.onload = function(event) {
-                const img = document.createElement('img');
-                img.src = event.target.result;
-                img.classList.add('preview-img');
-                photoUpload.innerHTML = '';
-                photoUpload.appendChild(img);
-            };
-
-            reader.readAsDataURL(file);
-        });
-
     }
+
+    // Changement des élements de la modale après que le bouton "Ajouter une photo soit pressé"
+    ajoutPhoto.addEventListener('click', (event) => {
+        modaleTitre.innerText = "Ajout photo";
+        ajoutPhoto.innerText = "Valider";
+        ajoutPhoto.style.backgroundColor = "#A7A7A7";
+        ajoutPhoto.style.cursor = "not-allowed"
+        listeBoites.style.display = "none";
+        formulaireModale.style.display = "block";
+        ajoutRetour();
+    });
+
+    const inputFormulaire = [inputTitre, inputPhoto, inputCateogorie];
+    inputFormulaire.forEach((input) => {
+        input.addEventListener('input', (event) => {
+            if (inputPhoto.value !== "" && inputTitre.value !== "" && inputCateogorie.value !== "") {
+                ajoutPhoto.style.backgroundColor = "#1d6154";
+                ajoutPhoto.style.cursor = "pointer"
+            } else {
+                ajoutPhoto.style.backgroundColor = "#A7A7A7";
+                ajoutPhoto.style.cursor = "not-allowed"
+            }
+        });
+    });
+
+    //Ajout de la photo sélectionné comme placeholder
+    inputPhoto.addEventListener('change', function(event) {
+        const file = event.target.files[0];
+        const reader = new FileReader();
+
+        reader.onload = function(event) {
+            const img = document.createElement('img');
+            img.src = event.target.result;
+            img.classList.add('preview-img');
+            photoUpload.innerHTML = '';
+            photoUpload.appendChild(img);
+        };
+
+        reader.readAsDataURL(file);
+    });
 }
 galleryModale()
